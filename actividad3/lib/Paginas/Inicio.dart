c16f068fe4c7b709d4c281986484f23088919634
xMarlonGx <marlonportillo41@gmail.com>
2022-04-30 15:25:48 -0600

    Primera carga

---
+++ actividad3/lib/Paginas/Inicio.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Inicio extends StatefulWidget {
+  Inicio({Key? key}) : super(key: key);
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
