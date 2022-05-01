7fbf5b98eae72dd13c8f1194a54e90d1b070af17
xMarlonGx <marlonportillo41@gmail.com>
2022-05-01 13:00:46 -0600

    Primera carga

--- actividad3/android/app/src/main/kotlin/com/example/actividad3/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.actividad3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ firebaseunidad3/android/app/src/main/kotlin/com/example/firebaseunidad3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebaseunidad3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
